543113e3689e40268b107462e70ed84852942fd3
augustopedro <augustopedrocouto@gmail.com>
2025-03-28 10:00:51 -0300

    add meal detais screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -7,9 +7,11 @@ class MealItem extends StatelessWidget {
   const MealItem({
     super.key,
     required this.meal,
+    required this.onSelectMeal,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String capitalize(String toCapitalize) => toCapitalize.isEmpty
       ? toCapitalize
@@ -25,7 +27,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
